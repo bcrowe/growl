266ba31a499599807b6f42ae561f75775b3ef3d1
Bryan Crowe <bryan@bryan-crowe.com>
2014-09-11 21:40:19 -0400

    Add constructor, growl, and createCommand methods

--- src/BryanCrowe/Growl.php
+++ src/BryanCrowe/Growl.php
@@ -3,5 +3,24 @@ namespace BryanCrowe;
 
 class Growl
 {
+    public function __construct() {}
 
+    public function growl($message = null, $options = []) {}
+
+    public function createCommand()
+    {
+        switch (PHP_OS) {
+            case 'Darwin':
+                $command = [];
+            break;
+            case 'Linux':
+                $command = [];
+            break;
+            case 'WINNT':
+                $command = [];
+            break;
+
+            return $command;
+        }
+    }
 }
