8513cd5a8a3e9917176cc329db7581468e5ca706
Bryan Crowe <bryan@bryan-crowe.com>
2014-09-12 16:40:41 -0400

    Prepare for testing

---
+++ tests/GrowlTest.php
@@ -0,0 +1,16 @@
+<?php
+use BryanCrowe\Growl;
+use \PHPUnit_Framework_TestCase;
+
+class GrowlTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->Growl = new Growl();
+    }
+
+    public function tearDown()
+    {
+
+    }
+}
