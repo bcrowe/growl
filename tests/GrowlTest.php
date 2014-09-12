b9df2420eadaf9e1869c13d688e53d2b0a38b012
Bryan Crowe <bryan@bryan-crowe.com>
2014-09-12 18:45:31 -0400

    Wrote a couple tests

--- tests/GrowlTest.php
+++ tests/GrowlTest.php
@@ -6,16 +6,77 @@ class GrowlTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
+        parent::setUp();
+
+        $this->argSets = [
+            'Darwin-Growl' => [
+                'type' => 'Darwin-Growl',
+                'pkg' => 'growlnotify',
+                'msg' => '-m',
+                'title' => '',
+                'sticky' => '--sticky'
+            ],
+            'Darwin-Notifier' => [
+                'type' => 'Darwin-Notifier',
+                'pkg' => 'terminal-notifier',
+                'msg' => '-message',
+                'title' => '-title',
+                'subtitle' => '-subtitle'
+            ],
+            'Linux-Growl' => [
+                'type' => 'Linux-Growl',
+                'pkg' => 'growl',
+                'msg' => '-m',
+                'title' => '-title',
+                'subtitle' => '-subtitle'
+            ],
+            'Linux-Notify' => [
+                'type' => 'Linux-Notify',
+                'pkg' => 'notify-send',
+                'msg' => '',
+                'sticky' => '-t 0'
+            ],
+            'Windows' => [
+                'type' => 'Windows',
+                'pkg' => 'growlnotify',
+                'msg' => '',
+                'title' => '/t:',
+                'sticky' => '/s:true'
+            ]
+        ];
+
         $this->Growl = new Growl();
     }
 
     public function tearDown()
     {
-
+        unset($this->argSets);
+        unset($this->Loader);
+        parent::tearDown();
     }
 
-    public function testGrowl()
+    public function testDarwinGrowl()
     {
-        $this->assertTrue(true);
+        $argSets = $this->argSets;
+
+        $expected = 'growlnotify -m "This is a message"  "LOL OMG" --sticky';
+        $options = ['title' => 'LOL OMG', 'subtitle' => 'Sub-guy', 'sticky' => true];
+        $result = $this->Growl->buildCommand('This is a message', $argSets['Darwin-Growl'], $options);
+        $this->assertEquals($expected, $result);
+
+        $expected = 'growlnotify -m "This is a message"  "LOL OMG"';
+        $options = ['title' => 'LOL OMG', 'subtitle' => 'Sub-guy'];
+        $result = $this->Growl->buildCommand('This is a message', $argSets['Darwin-Growl'], $options);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testTerminalNotifier()
+    {
+        $argSets = $this->argSets;
+
+        $expected = 'terminal-notifier -message "This is a message" -title "LOL OMG" -subtitle "Sub-guy"';
+        $options = ['title' => 'LOL OMG', 'subtitle' => 'Sub-guy', 'sticky' => true];
+        $result = $this->Growl->buildCommand('This is a message', $argSets['Darwin-Notifier'], $options);
+        $this->assertEquals($expected, $result);
     }
 }
