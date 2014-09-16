fcc8d1bfd0699585b8b8ea3bb76f9c1357f1c01f
Bryan Crowe <bryan@bryan-crowe.com>
2014-09-15 20:50:24 -0400

    Prepare for Monolog support

---
+++ src/BryanCrowe/Monolog/Formatter/GrowlFormatter.php
@@ -0,0 +1,9 @@
+<?php
+namespace BryanCrowe\Monolog\Formatter;
+
+use Monolog\Formatter\LineFormatter;
+
+class GrowlFormatter extends LineFormatter
+{
+    const SIMPLE_FORMAT = '';
+}
---
+++ src/BryanCrowe/Monolog/Handler/GrowlHandler.php
@@ -0,0 +1,10 @@
+<?php
+namespace BryanCrowe\Monolog\Handler;
+
+use BryanCrowe\Monolog\Formatter\GrowlFormatter;
+use Monolog\Handler\AbstractProcessingHandler;
+
+class GrowlHandler extends AbstractProcessingHandler
+{
+
+}
