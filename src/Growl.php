c9852a6ebc8d330e21759b60ff162015c07a2a15
Bryan Crowe <bryan@bryan-crowe.com>
2014-11-26 18:29:55 -0500

    Initial stub of Growl class

--- src/Growl.php
+++ src/Growl.php
@@ -4,5 +4,23 @@ namespace BryanCrowe\Growl;
 
 class Growl
 {
+    protected $builder;
 
+    protected $args = array();
+
+    public function __construct($builder)
+    {
+        $this->builder = $builder;
+    }
+
+    public function execute()
+    {
+        $command = $this->builder->build($args);
+        return exec($command);
+    }
+
+    public function __call($name, $args)
+    {
+        $this->args[$name] = $args[0];
+    }
 }
