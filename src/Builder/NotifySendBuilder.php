06454efcb5cb365eba3722afad26e6a0711809a0
Bryan Crowe <bryan@bryan-crowe.com>
2014-11-28 17:13:29 -0500

    Build basic notify-send commands

--- src/Builder/NotifySendBuilder.php
+++ src/Builder/NotifySendBuilder.php
@@ -9,5 +9,17 @@ class NotifySendBuilder extends BuilderAbstract
     public function build($args)
     {
         $command = self::PROGRAM;
+
+        if (isset($args['title'])) {
+            $command .= ' ' . $this->quotify($args['title']);
+        }
+        if (isset($args['message'])) {
+            $command .= ' ' . $this->quotify($args['message']);
+        }
+        if (isset($args['sticky']) && $args['sticky'] === true) {
+            $command .= ' -t 0';
+        }
+
+        return $command;
     }
 }
