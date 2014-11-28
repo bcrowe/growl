ddcc703d580ffcdcd21a34eed1e1973462e57afc
Bryan Crowe <bryan@bryan-crowe.com>
2014-11-28 17:07:32 -0500

    Build basic terminal-notifier commands

--- src/Builder/TerminalNotifierBuilder.php
+++ src/Builder/TerminalNotifierBuilder.php
@@ -9,5 +9,17 @@ class TerminalNotifierBuilder extends BuilderAbstract
     public function build($args)
     {
         $command = self::PROGRAM;
+
+        if (isset($args['title'])) {
+                $command .= ' -title ' . $this->quotify($args['title']);
+            }
+        if (isset($args['subtitle'])) {
+            $command .= ' -subtitle ' . $this->quotify($args['subtitle']);
+        }
+        if (isset($args['message'])) {
+            $command .= ' -message ' . $this->quotify($args['message']);
+        }
+
+        return $command;
     }
 }
