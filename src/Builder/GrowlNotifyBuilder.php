54da6fb6271df877c84992ca8d3cf574fd5b6132
Bryan Crowe <bryan@bryan-crowe.com>
2014-11-28 16:06:24 -0500

    Build basic growlnotify commands

--- src/Builder/GrowlNotifyBuilder.php
+++ src/Builder/GrowlNotifyBuilder.php
@@ -11,11 +11,31 @@ class GrowlNotifyBuilder extends BuilderAbstract
         $command = self::PROGRAM;
 
         if (PHP_OS === 'Darwin') {
+            if (isset($args['title'])) {
+                $command .= ' -t' . $this->quotify($args['title']);
+            }
+            if (isset($args['message'])) {
+                $command .= ' -m ' . $this->quotify($args['message']);
+            }
+            if (isset($args['sticky']) && $args['sticky'] === true) {
+                $command .= ' --sticky';
+            }
 
+            return $command;
         }
 
         if (PHP_OS === 'WINNT') {
+            if (isset($args['title'])) {
+                $command .= ' /t:' . $this->quotify($args['title']);
+            }
+            if (isset($args['sticky']) && $args['sticky'] === true) {
+                $command .= ' /s:true';
+            }
+            if (isset($args['message'])) {
+                $command .= ' ' . $this->quotify($args['message']);
+            }
 
+            return $command;
         }
     }
 }
