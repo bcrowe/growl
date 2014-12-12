a38ba0e89a92738474092376cfe5451d4772e261
Bryan Crowe <bryan@bryan-crowe.com>
2014-12-12 01:26:18 -0500

    Add Growl Windows test

---
+++ tests/GrowlNotifyWindowsBuilderTest.php
@@ -0,0 +1,39 @@
+<?php
+use BryanCrowe\Growl\Builder\GrowlNotifyWindowsBuilder;
+use \PHPUnit_Framework_TestCase;
+
+class GrowlNotifyWindowsBuilderTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->GrowlNotifyWindowsBuilder = new GrowlNotifyWindowsBuilder();
+    }
+
+    public function tearDown()
+    {
+        unset($this->GrowlNotifyWindowsBuilder);
+        parent::tearDown();
+    }
+
+    public function testBuild()
+    {
+        $options = array(
+            'title' => 'Hello',
+            'message' => 'World',
+            'sticky' => true
+        );
+        $expected = "growlnotify /t:'Hello' /s:true 'World'";
+        $result = $this->GrowlNotifyWindowsBuilder->build($options);
+        $this->assertSame($expected, $result);
+
+        $options = array(
+            'title' => 'Hello',
+            'message' => 'World',
+            'sticky' => false
+        );
+        $expected = "growlnotify /t:'Hello' 'World'";
+        $result = $this->GrowlNotifyWindowsBuilder->build($options);
+        $this->assertSame($expected, $result);
+    }
+}
