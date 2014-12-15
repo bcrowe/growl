65509ab7c3e61db1224ef593b6ee6ce956c5b602
Bryan Crowe <bryan@bryan-crowe.com>
2014-12-14 22:51:47 -0500

    Test Growl set and escape methods

--- tests/GrowlTest.php
+++ tests/GrowlTest.php
@@ -17,8 +17,31 @@ class GrowlTest extends PHPUnit_Framework_TestCase
         parent::tearDown();
     }
 
-    public function testGrowl()
+    public function testSet()
     {
-        $this->assertTrue(true);
+        $expected = array(
+            'hello' => 'world',
+            'title' => 'Hey',
+            'message' => 'Whatsup?'
+        );
+        $result = $this->Growl->set('hello', 'world')
+            ->set('title', 'Hey')
+            ->set('message', 'Whatsup?');
+
+        $this->assertEquals($expected, PHPUnit_Framework_Assert::readAttribute($result, 'options'));
+    }
+
+    public function testEscape()
+    {
+        $expected = array(
+            'hello' => '\'world\''
+        );
+
+        $growl = new Growl(new GrowlNotifyBuilder());
+        $growlReflection = new ReflectionClass($growl);
+        $method = $growlReflection->getMethod('escape');
+        $method->setAccessible(true);
+     
+        $this->assertEquals($expected, $method->invokeArgs($growl, array(array('hello' => 'world'))));
     }
 }
