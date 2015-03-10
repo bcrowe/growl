bcd478f84779897ec5159d9d6db9a2fb40bf00fc
Bryan Crowe <bryan@bryan-crowe.com>
2015-03-09 21:24:53 -0400

    Stay within 80 columns

--- src/Builder/BuilderAbstract.php
+++ src/Builder/BuilderAbstract.php
@@ -28,13 +28,16 @@ abstract class BuilderAbstract implements BuilderInterface
             return;
         }
 
-        throw new InvalidArgumentException('This constructor expects a string argument.');
+        throw new InvalidArgumentException(
+            'This constructor expects a string argument.'
+        );
     }
 
     /**
      * Build the command string to be executed.
      *
-     * @param array $options An array of options to use for building the command.
+     * @param array $options An array of options to use for building the
+     * command.
      * @return string
      */
     abstract public function build($options);
